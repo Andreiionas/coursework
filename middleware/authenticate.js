aac1486eb4f913ab1d6bf77ab327dadb456e9e38
Andreiionas <andreiionas009@gmail.com>
2024-12-08 16:08:11 +0000

    init

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,7 +4,7 @@ const authenticate = (req, res, next) => {
   const token = req.header('Authorization')?.replace('Bearer ', '');
 
   if (!token) return res.status(401).json({ message: 'No token provided, access denied' });
-
+              
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // Store the decoded user ID in the request
